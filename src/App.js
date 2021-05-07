6b21680737f11ef4213fb4da96d5cefc221f5c5b
gaurvimalan <gaurvimalan15@gmail.com>
2021-05-07 16:25:18 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Provider} from "react-redux";
 import './App.css';
+import store from './store/index';
+import Users from './Users';
+import Form from './Form';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+    <div className="container">
+        <Form/>
+        <Users/>
     </div>
-  );
+    </Provider>
+  )
 }
 
 export default App;
---
+++ src/store/reducers/apiUsersReducer.js
@@ -0,0 +1,8 @@
+const initState = {allUsers:[]}
+const apiUsersReducer = (state=initState, action) =>{
+	switch(action.type){
+		case 'STORE_USERS':
+		default:
+		return state;
+	}
+}
